9f3509c6adde1831b67910e6db1d8408cd8fe2ef
Nick Spera <nickaspera@gmail.com>
2019-11-06 18:39:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function nowServing(line){
 }
 
 function currentLine(katzDeliLine) {
-    for (var i = 0; i < katzDeliLine.length; i++) {
-      return (`The line is currently: ${i + 1}. ${katzDeliLine[i]}` )
+    for (var i = 0; i < katzDeliLine; i++) {
+      return (`The line is currently: ${i}. ${katzDeliLine[i]}`)
     }
       return "The line is currently empty."
 }
